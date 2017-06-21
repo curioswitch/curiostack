06976892b96dc782f31fc70a0d7d4267bc15a5b7
Choko <choko@curioswitch.org>
2017-06-21 20:49:44 +0900

    Migrate to redux-actions.

--- common/web/boilerplate/internals/generators/container/index.js
+++ common/web/boilerplate/internals/generators/container/index.js
@@ -103,13 +103,6 @@ module.exports = {
         abortOnFail: true,
       });
 
-      // Constants
-      actions.push({
-        type: 'add',
-        path: '../../app/containers/{{properCase name}}/constants.js',
-        templateFile: './container/constants.js.hbs',
-        abortOnFail: true,
-      });
 
       // Selectors
       actions.push({
