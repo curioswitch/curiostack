8c2199051bc3de39c50d6ed9dca235623c374ca4
Ryo Kawaguchi <ryo@kawagu.ch>
2017-07-13 17:37:22 +0900

    Expose root privilege of the dev db. (#8)
    * Expose root privilege of the dev db.
    
    * Use devAdminPassword for MYSQL_ROOT_PASSWORD
    
    * Fix typo.

--- tools/gradle-plugins/gradle-curiostack-plugin/src/main/java/org/curioswitch/gradle/plugins/gcloud/CurioDatabasePlugin.java
+++ tools/gradle-plugins/gradle-curiostack-plugin/src/main/java/org/curioswitch/gradle/plugins/gcloud/CurioDatabasePlugin.java
@@ -67,9 +67,9 @@ public class CurioDatabasePlugin implements Plugin<Project> {
     String devAdminPassword = config.devAdminPassword();
     if (devAdminPassword != null) {
       generateDevDbDockerfile.environmentVariable("MYSQL_PASSWORD", devAdminPassword);
+      // Root privilege needs to be exposed to create users other than MYSQL_USER.
+      generateDevDbDockerfile.environmentVariable("MYSQL_ROOT_PASSWORD", devAdminPassword);
     }
-    generateDevDbDockerfile.environmentVariable("MYSQL_RANDOM_ROOT_PASSWORD", "yes");
-    generateDevDbDockerfile.environmentVariable("MYSQL_ONETIME_PASSWORD", "yes");
 
     DockerBuildImage buildDevDbDockerImage =
         project.getTasks().create("buildDevDbDockerImage", DockerBuildImage.class);
