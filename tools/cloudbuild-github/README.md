43fa31e63d74c3c7cc4b278471868bdd0bfb5194
Choko <choko@curioswitch.org>
2017-12-31 20:23:47 +0900

    Add a CLI for setting up cloudbuild-github.

--- tools/cloudbuild-github/README.md
+++ tools/cloudbuild-github/README.md
@@ -3,9 +3,53 @@
 cloudbuild-github allows integrating Google Container Builder (Cloudbuild) and GitHub. It sets up a
 webhook and status / comment notifications using Google Cloud Functions.
 
-Example usage at https://github.com/curioswitch/curiostack/tree/master/tools/cloudbuild-github-configured
+## Setup
 
-Eventually this package will include a CLI to make generating `config.yml` and `index.js` easier,
-but for now the appropriate usage is to follow the example to make the two files.
+### Curiostack users
 
-TODO(choko): Add a configuration CLI and more documentation.
+Coming soon: A gradle task that does all above the below automatically.
+
+### Normal usage
+
+The `gcloud` must be present on the path (see https://cloud.google.com/sdk/downloads) for how to 
+install. In addition, the `GCLOUD_PROJECT` environment variable may need to be set to your GCP
+project id.
+
+Start by creating a new project with a dependency on the library.
+
+```bash
+$ yarn init # main: index.js, private: true
+$ yarn add @curiostack/cloudbuild-github
+$ yarn
+```
+
+This will download the library, including a CLI to help setup. To create the configured cloud function
+and deploy it, run
+
+```bash
+$ yarn run cloudbuild-cli
+```
+
+This will prompt you for information about your repository, including an access token for working with
+your GitHub repository (see details about tokens [here](https://help.github.com/articles/creating-a-personal-access-token-for-the-command-line/)).
+It will then write out an `index.js` and `config.yml` file that will be used by the webhook, deploy
+the cloud functions, and set up the repository webhook. That's it!
+
+The current version does not support an easy flow for editing configuration yet (coming soon). In the
+meantime, especially for modifying the build step to match your build system, edit `config.yml` and
+redeploy manually using
+
+```bash
+$ yarn run build
+$ gcloud beta functions deploy cloudbuildGithubWebhook --trigger-http
+$ gcloud beta functions deploy cloudbuildGithubNotifier --trigger-topic cloud-builds
+```
+
+Note, I have been unsuccessful in having new code be recognized when redeploying (bug in cloud functions?).
+To work around this, you may need to delete the functions before redeploying - this will introduce
+downtime between the delete and deploy.
+
+```bash
+$ gcloud beta functions delete cloudbuildGithubWebhook
+$ gcloud beta functions delete cloudbuildGithubNotifier
+```
---
+++ tools/cloudbuild-github/src/cli/setup.js
@@ -0,0 +1,306 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017 Choko (choko@curioswitch.org)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+// @flow
+
+import crypto from 'crypto';
+import fs from 'fs';
+
+import { spawn } from 'child-process-promise';
+import inquirer from 'inquirer';
+import request from 'request-promise-native';
+import yaml from 'js-yaml';
+
+import { googleApis } from '../gcloud';
+
+const KMS_LOCATIONS = [
+  'global',
+  'asia-east1',
+  'asia-southeast1',
+  'europe-west1',
+  'us-central1',
+  'us-east1',
+  'us-west1',
+];
+
+const fetchWithMergeScript = `echo 'echo $$GITHUB_TOKEN' > /tmp/cloudbuild-github-pass.sh && \\
+chmod +x /tmp/cloudbuild-github-pass.sh && \\
+git init && \\
+git fetch --no-tags --progress $_REPOSITORY_URL +$_OTHER_BRANCH_REMOTE_REF:$_OTHER_BRANCH_LOCAL_REF +$_BASE_BRANCH_REMOTE_REF:$_BASE_BRANCH_LOCAL_REF && \\
+git config remote.origin.url $_REPOSITORY_URL && \\
+git config user.name cloudbuild && \\
+git config user.email cloudbuild@example.com && \\
+git checkout -f $_OTHER_BRANCH_LOCAL_REF && \\
+git merge --no-ff --no-edit --no-progress $_BASE_BRANCH_LOCAL_REF && \\
+MERGE_REV=$(git rev-parse HEAD^{commit}) && \\
+git checkout -f $_BASE_BRANCH_LOCAL_REF && \\
+git merge --no-ff --no-edit --no-progress $$MERGE_REV && \\
+MERGE_REV=$(git rev-parse HEAD^{commit}) && \\
+git checkout -f $$MERGE_REV
+`;
+
+const indexJs = `/*
+ * MIT License
+ *
+ * Copyright (c) 2017 Choko (choko@curioswitch.org)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+module.exports = require('@curiostack/cloudbuild-github');
+`;
+
+const GITHUB_API_BASE = 'https://api.github.com';
+
+async function makeGithubRequest(
+  uri: string,
+  body: any,
+  token: string,
+  method: string = 'POST',
+) {
+  return request({
+    method,
+    uri,
+    body,
+    json: true,
+    headers: {
+      'User-Agent': 'cloudbuild-github',
+    },
+    auth: {
+      user: 'token',
+      pass: token,
+    },
+  });
+}
+
+export default async function setup() {
+  const ui = new inquirer.ui.BottomBar();
+
+  const answers = await inquirer.prompt([
+    {
+      name: 'kms.location',
+      message: 'Enter the location for the keyring used to encrypt secrets.',
+      default: 'us-central1',
+      validate: (val) => KMS_LOCATIONS.includes(val) || 'Invalid KMS location.',
+    },
+    {
+      name: 'kms.keyring',
+      message:
+        'Enter the name of the keyring used to encrypt secrets. ' +
+        'Will be created if it does not exist.',
+      default: 'cloudbuild',
+    },
+    {
+      name: 'kms.key',
+      message:
+        'Enter the name of the key used to encrypt secrets. ' +
+        'Will be created if it does not exist.',
+      default: 'github',
+    },
+    {
+      name: 'repo.name',
+      message: 'Enter the name of the GitHub repository.',
+      validate: (val) => !!val || 'GitHub repository name is required.',
+    },
+    {
+      name: 'repo.token',
+      message:
+        'Paste-in a GitHub personal access token with repo and hook permissions ' +
+        '- generate at "https://github.com/settings/tokens/new?scopes=repo,admin:repo_hook&description=cloudbuild"',
+      validate: (val) => !!val || 'Access token is required.',
+    },
+  ]);
+
+  const { location, keyring, key } = answers.kms;
+
+  const webhookSecret = crypto.randomBytes(10).toString('hex');
+  const projectId = await googleApis.getProjectId();
+  if (!projectId) {
+    ui.log.write(
+      'Could not determine project id, try setting the GCLOUD_PROJECT environment variable',
+    );
+    return;
+  }
+
+  ui.log.write('Setting up keys.');
+  try {
+    await googleApis.createKeyring(location, keyring);
+  } catch (err) {
+    if (err.errors[0].reason !== 'alreadyExists') {
+      throw err;
+    }
+  }
+  try {
+    await googleApis.createKey(location, keyring, key);
+  } catch (err) {
+    if (err.errors[0].reason !== 'alreadyExists') {
+      throw err;
+    }
+  }
+  await googleApis.setDecrypter(
+    location,
+    keyring,
+    `${projectId}@appspot.gserviceaccount.com`,
+  );
+
+  ui.log.write('Encrypting secrets.');
+  const encryptedWebhookSecret = await googleApis.encryptKey(
+    location,
+    keyring,
+    key,
+    webhookSecret,
+  );
+  const encryptedGithubToken = await googleApis.encryptKey(
+    location,
+    keyring,
+    key,
+    answers.repo.token,
+  );
+
+  const config = {
+    kms: answers.kms,
+    encryptedWebhookSecret,
+    repos: {
+      [answers.repo.name]: {
+        encryptedGithubToken,
+        cloudbuild: {
+          steps: [
+            {
+              id: 'fetch-source',
+              // Use gcloud image instead of git, since the latter prevents GIT_ASKPASS from working.
+              name: 'gcr.io/cloud-builders/gcloud',
+              entrypoint: 'bash',
+              args: ['-c', fetchWithMergeScript],
+              env: ['GIT_ASKPASS=/tmp/cloudbuild-github-pass.sh'],
+              secretEnv: ['GITHUB_TOKEN'],
+            },
+            {
+              id: 'build-all',
+              name: 'gcr.io/$PROJECT_ID/java-cloud-builder',
+              entrypoint: './gradlew',
+              args: ['continuousTest', '--stacktrace', '--no-daemon'],
+              env: ['CI=true'],
+            },
+          ],
+          timeout: '60m',
+          secrets: [
+            {
+              kmsKeyName: `projects/${projectId}/locations/${
+                answers.kms.location
+              }/keyRings/${answers.kms.keyring}/cryptoKeys/${answers.kms.key}`,
+              secretEnv: {
+                GITHUB_TOKEN: encryptedGithubToken,
+              },
+            },
+          ],
+        },
+      },
+    },
+  };
+  const configYaml = yaml.safeDump(config, {
+    // It's easier to manage build scripts without worrying about line length.
+    lineWidth: 100000,
+  });
+
+  ui.log.write('Outputting config.yml and index.js.');
+  fs.writeFileSync('index.js', indexJs);
+  fs.writeFileSync('config.yml', configYaml);
+
+  ui.log.write('Deploying cloud functions.');
+  await spawn(
+    'gcloud',
+    [
+      'beta',
+      'functions',
+      'deploy',
+      'cloudbuildGithubWebhook',
+      '--trigger-http',
+    ],
+    { stdio: 'inherit' },
+  );
+  await spawn(
+    'gcloud',
+    [
+      'beta',
+      'functions',
+      'deploy',
+      'cloudbuildGithubNotifier',
+      '--trigger-topic',
+      'cloud-builds',
+    ],
+    { stdio: 'inherit' },
+  );
+
+  const webhookUrl = `https://us-central1-${projectId}.cloudfunctions.net/cloudbuildGithubWebhook`;
+
+  ui.log.write('Setting up repository webhook');
+  const hooksUri = `${GITHUB_API_BASE}/repos/${answers.repo.name}/hooks`;
+  const existingHooks = await makeGithubRequest(
+    hooksUri,
+    null,
+    answers.repo.token,
+    'GET',
+  );
+  await Promise.all(
+    existingHooks
+      .filter((hook) => hook.config.url === webhookUrl)
+      .map((hook) =>
+        makeGithubRequest(
+          `${hooksUri}/${hook.id}`,
+          null,
+          answers.repo.token,
+          'DELETE',
+        ),
+      ),
+  );
+
+  const hook = {
+    name: 'web',
+    config: {
+      url: webhookUrl,
+      content_type: 'json',
+      secret: webhookSecret,
+    },
+    events: ['pull_request'],
+  };
+  await makeGithubRequest(hooksUri, hook, answers.repo.token);
+
+  ui.log.write('Done!');
+}
