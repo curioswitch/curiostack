d06a1d59ad9927ce83f3d690f4c0d8d7415c0e1c
Choko <choko@curioswitch.org>
2018-01-04 14:23:49 +0900

    Recompile server framework with latest deps

--- tools/gradle-plugins/gradle-curiostack-plugin/src/main/java/org/curioswitch/gradle/plugins/curiostack/StandardDependencies.java
+++ tools/gradle-plugins/gradle-curiostack-plugin/src/main/java/org/curioswitch/gradle/plugins/curiostack/StandardDependencies.java
@@ -276,7 +276,7 @@ class StandardDependencies {
               .build(),
           ImmutableDependencySet.builder()
               .group("org.curioswitch.curiostack")
-              .version("0.0.36")
+              .version("0.0.37")
               .addModules("curio-server-framework")
               .build(),
           ImmutableDependencySet.builder()
