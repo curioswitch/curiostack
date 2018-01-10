6f69b4623f0f1f24cc50827b186d743ad2c8990e
Choko <choko@curioswitch.org>
2018-01-10 16:11:43 +0900

    Allow setting cache name and fix codec.

--- tools/gradle-plugins/gradle-curiostack-plugin/src/main/java/org/curioswitch/gradle/plugins/curiostack/StandardDependencies.java
+++ tools/gradle-plugins/gradle-curiostack-plugin/src/main/java/org/curioswitch/gradle/plugins/curiostack/StandardDependencies.java
@@ -286,7 +286,7 @@ class StandardDependencies {
               .build(),
           ImmutableDependencySet.builder()
               .group("org.curioswitch.curiostack")
-              .version("0.0.38")
+              .version("0.0.39")
               .addModules("curio-server-framework")
               .build(),
           ImmutableDependencySet.builder()
