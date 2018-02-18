11b65134e6a5d32b1716addb84ef1003883a7800
Choko <choko@curioswitch.org>
2018-02-18 15:09:52 +0900

    Update license

--- common/server/framework/src/main/java/org/curioswitch/common/server/framework/auth/firebase/FirebaseAuthConfig.java
+++ common/server/framework/src/main/java/org/curioswitch/common/server/framework/auth/firebase/FirebaseAuthConfig.java
@@ -1,7 +1,7 @@
-/*
+/**
  * MIT License
  *
- * Copyright (c) 2017 Choko (choko@curioswitch.org)
+ * Copyright (c) 2018 Choko (choko@curioswitch.org)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -21,7 +21,6 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-
 package org.curioswitch.common.server.framework.auth.firebase;
 
 import static com.google.common.base.Preconditions.checkArgument;
