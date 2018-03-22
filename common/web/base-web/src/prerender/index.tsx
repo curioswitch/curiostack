f0262446713c7b6483f6a4713e786d793cb957fb
Choko <choko@curioswitch.org>
2018-03-22 17:18:34 +0900

    v0.0.11-alpha.1

---
+++ common/web/base-web/src/prerender/index.tsx
@@ -0,0 +1,102 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018 Choko (choko@curioswitch.org)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+// We use webpack require to avoid exposing the web dependencies to typescript, as we don't want
+// to transpile them.
+// tslint:disable:no-var-requires
+
+import {
+  createMemoryHistory as createHistory,
+  LocationDescriptor,
+} from 'history';
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import { Helmet } from 'react-helmet';
+import { Provider } from 'react-redux';
+import { StaticRouter } from 'react-router-dom';
+import { Store } from 'redux';
+import { ServerStyleSheet, ThemeProvider } from 'styled-components';
+
+const appConfig = require('../../../app').appConfig;
+const LanguageProvider = require('../../../containers/LanguageProvider');
+const initRedux = require('../../../redux/init');
+
+import Template from './template';
+
+interface Props {
+  messages: any;
+  store: Store;
+  location: LocationDescriptor;
+  Component: React.ComponentClass<any> | React.StatelessComponent<any>;
+  theme: any;
+}
+
+function RenderedPage({ messages, store, location, Component, theme }: Props) {
+  return (
+    <Provider store={store}>
+      <LanguageProvider messages={messages}>
+        <ThemeProvider theme={theme || {}}>
+          <StaticRouter location={location} context={{}}>
+            <Component />
+          </StaticRouter>
+        </ThemeProvider>
+      </LanguageProvider>
+    </Provider>
+  );
+}
+
+export default function(locals: any) {
+  const path = locals.path.replace('.html', '');
+  // Create redux store with history
+  const initialState = {
+    ...appConfig.initialState,
+    route: {
+      location: {
+        pathname: path,
+      },
+    },
+  };
+  const history = createHistory();
+  const store = initRedux(initialState, history);
+
+  const sheet = new ServerStyleSheet();
+  const page = sheet.collectStyles(
+    <RenderedPage
+      messages={appConfig.messages}
+      store={store}
+      location={initialState.route.location}
+      Component={appConfig.component}
+      theme={appConfig.theme || {}}
+    />,
+  );
+  const renderedContent = ReactDOMServer.renderToString(page);
+  const helmet = Helmet.renderStatic();
+  return ReactDOMServer.renderToStaticMarkup(
+    <Template
+      content={renderedContent}
+      mainScriptSrc={locals.assets.main}
+      helmet={helmet}
+    />,
+  );
+}
