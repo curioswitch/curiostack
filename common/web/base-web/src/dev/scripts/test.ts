2efedc9cf016645b0eda1bf318dd754c05f2d2b2
Choko <choko@curioswitch.org>
2018-04-12 15:45:02 +0900

    Add a test script to base-web.

---
+++ common/web/base-web/src/dev/scripts/test.ts
@@ -0,0 +1,61 @@
+#!/usr/bin/env node
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018 Choko (choko@curioswitch.org)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import { run } from 'jest-cli';
+
+const CONFIG = {
+  globals: {
+    'ts-jest': {
+      babelConfig: {
+        presets: ['stage-0', 'react'],
+        plugins: [
+          [
+            'react-intl-auto',
+            {
+              removePrefix: 'src/',
+            },
+          ],
+        ],
+      },
+    },
+  },
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  transformIgnorePatterns: ['node_modules/(?!@curiostack)'],
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupTestFrameworkScriptFile:
+    '@curiostack/base-web/build/dev/testing/test-bundler.jsx',
+  snapshotSerializers: ['enzyme-to-json/serializer'],
+  moduleNameMapper: {
+    '.*\\.(css|less|styl|scss|sass)$':
+      '@curiostack/base-web/build/dev/testing/mocks/cssModule.js',
+    '.*\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '@curiostack/base-web/build/dev/testing/mocks/image.js',
+  },
+};
+
+run(['--config', JSON.stringify(CONFIG)]).catch(() => process.exit(1));
