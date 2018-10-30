cda1e2a7fa68cf21fd118be2de7f3f911d0e4b00
Choko <choko@curioswitch.org>
2018-10-30 20:50:27 +0900

    Pass parameters to shims

--- tools/gradle-plugins/gradle-tool-downloader-plugin/src/main/resources/tooldownloader/shim-template.sh
+++ tools/gradle-plugins/gradle-tool-downloader-plugin/src/main/resources/tooldownloader/shim-template.sh
@@ -3,8 +3,8 @@
 MARKER=||REPO_PATH||/.gradle/is-curiostack.txt
 
 if [ -f "$MARKER" ]; then
-  ||TOOL_EXE_PATH||
+  ||TOOL_EXE_PATH|| "$@"
 else
   export PATH=$(p=$(echo $PATH | tr ":" "\n" | grep -v "||SHIMS_PATH||" | tr "\n" ":"); echo ${p%:})
-  ||EXE_NAME||
+  ||EXE_NAME|| "$@"
 fi
