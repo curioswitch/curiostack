70e0dff765b75a8b8845ea6282b7a74cca8a9554
Choko <choko@curioswitch.org>
2018-11-06 19:17:00 +0900

    Check CI variable

--- gradle/get-jdk.sh
+++ gradle/get-jdk.sh
@@ -1,57 +1,60 @@
 #!/bin/bash
 
-# OS specific support (must be 'true' or 'false').
-windows=false
-darwin=false
-linux=false
-case "`uname`" in
-  CYGWIN* )
-    windows=true
-    ;;
-  Darwin* )
-    darwin=true
-    ;;
-  MINGW* )
-    windows=true
-    ;;
-  Linux* )
-    linux=true
-    ;;
-esac
+if [ -z "$CI" ]; then
 
-GRADLE_HOME="${GRADLE_USER_HOME:-${HOME}/.gradle}"
-OPENJDK_DIR="$GRADLE_HOME/curiostack/openjdk"
+  # OS specific support (must be 'true' or 'false').
+  windows=false
+  darwin=false
+  linux=false
+  case "`uname`" in
+    CYGWIN* )
+      windows=true
+      ;;
+    Darwin* )
+      darwin=true
+      ;;
+    MINGW* )
+      windows=true
+      ;;
+    Linux* )
+      linux=true
+      ;;
+  esac
 
-export JAVA_HOME="$OPENJDK_DIR/jdk-11.0.1"
+  GRADLE_HOME="${GRADLE_USER_HOME:-${HOME}/.gradle}"
+  OPENJDK_DIR="$GRADLE_HOME/curiostack/openjdk"
+
+  export JAVA_HOME="$OPENJDK_DIR/jdk-11.0.1"
 
 
-if "$linux" = "true"; then
-  SRC="https://download.java.net/java/GA/jdk11/13/GPL/openjdk-11.0.1_linux-x64_bin.tar.gz"
-  DEST="$OPENJDK_DIR/openjdk-11.0.1_linux-x64_bin.tar.gz"
-fi
+  if "$linux" = "true"; then
+    SRC="https://download.java.net/java/GA/jdk11/13/GPL/openjdk-11.0.1_linux-x64_bin.tar.gz"
+    DEST="$OPENJDK_DIR/openjdk-11.0.1_linux-x64_bin.tar.gz"
+  fi
 
-if "$darwin" = "true"; then
-  SRC="https://download.java.net/java/GA/jdk11/13/GPL/openjdk-11.0.1_osx-x64_bin.tar.gz"
-  DEST="$OPENJDK_DIR/openjdk-11.0.1_osx-x64_bin.tar.gz"
-fi
-
-if "$windows" = "true"; then
-  SRC="https://download.java.net/java/GA/jdk11/13/GPL/openjdk-11.0.1_windows-x64_bin.zip"
-  DEST="$OPENJDK_DIR/openjdk-11.0.1_osx-x64_bin.tar.gz"
-fi
-
-if [ ! -d "$JAVA_HOME" ]; then
-  mkdir -p "$OPENJDK_DIR"
-
-  echo "Downloading OpenJDK"
-  curl "$SRC" -o "$DEST"
-
-  cd "$OPENJDK_DIR"
+  if "$darwin" = "true"; then
+    SRC="https://download.java.net/java/GA/jdk11/13/GPL/openjdk-11.0.1_osx-x64_bin.tar.gz"
+    DEST="$OPENJDK_DIR/openjdk-11.0.1_osx-x64_bin.tar.gz"
+  fi
 
   if "$windows" = "true"; then
-    unzip "$DEST"
-  else
-    tar -xf "$DEST"
+    SRC="https://download.java.net/java/GA/jdk11/13/GPL/openjdk-11.0.1_windows-x64_bin.zip"
+    DEST="$OPENJDK_DIR/openjdk-11.0.1_osx-x64_bin.tar.gz"
   fi
-fi
 
+  if [ ! -d "$JAVA_HOME" ]; then
+    mkdir -p "$OPENJDK_DIR"
+
+    echo "Downloading OpenJDK"
+    curl "$SRC" -o "$DEST"
+
+    cd "$OPENJDK_DIR"
+
+    if "$windows" = "true"; then
+      unzip "$DEST"
+    else
+      tar -xf "$DEST"
+    fi
+  fi
+
+fi
