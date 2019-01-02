3e4b64258dddc1d8f157d038b7ca6af5c42e9c3d
Choko <choko@curioswitch.org>
2019-01-02 18:15:41 +0900

    Make codelabs tasks cacheable to avoid slow download of deps every time.

--- tools/gradle-plugins/gradle-curiostack-plugin/src/main/java/org/curioswitch/gradle/plugins/codelabs/CodelabsPlugin.java
+++ tools/gradle-plugins/gradle-curiostack-plugin/src/main/java/org/curioswitch/gradle/plugins/codelabs/CodelabsPlugin.java
@@ -24,17 +24,15 @@
 
 package org.curioswitch.gradle.plugins.codelabs;
 
-import static com.google.common.collect.ImmutableList.toImmutableList;
-
-import java.io.File;
-import java.util.List;
 import org.curioswitch.gradle.helpers.platform.PathUtil;
+import org.curioswitch.gradle.plugins.codelabs.tasks.DownloadDepsTask;
+import org.curioswitch.gradle.plugins.codelabs.tasks.ExportDocsTask;
 import org.curioswitch.gradle.plugins.nodejs.NodePlugin;
 import org.curioswitch.gradle.tooldownloader.DownloadedToolManager;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.plugins.BasePlugin;
-import org.gradle.api.tasks.Exec;
+import org.gradle.api.tasks.Copy;
 
 public class CodelabsPlugin implements Plugin<Project> {
 
@@ -51,54 +49,37 @@ public class CodelabsPlugin implements Plugin<Project> {
             .getTasks()
             .register(
                 "exportDocs",
-                Exec.class,
+                ExportDocsTask.class,
                 t -> {
-                  t.getOutputs().dir("build/claat");
-
-                  t.executable(claatPath);
-
                   var mdFileTree = project.fileTree("src");
                   mdFileTree.exclude("build").include("**/*.md");
 
-                  t.getInputs().files(mdFileTree);
-
-                  List<String> mdFiles =
-                      mdFileTree
-                          .getFiles()
-                          .stream()
-                          .map(File::getAbsolutePath)
-                          .collect(toImmutableList());
-
-                  t.args("export");
-                  t.args(mdFiles);
-
-                  t.workingDir("build/claat");
+                  t.getMdFiles().set(mdFileTree);
+                  t.getOutputDir().set(project.file("build/exported"));
                 });
-    var buildSite =
+    var downloadDeps =
         project
             .getTasks()
             .register(
-                "buildSite",
-                Exec.class,
+                "downloadDeps",
+                DownloadDepsTask.class,
                 t -> {
-                  t.dependsOn(exportDocs);
-                  t.getInputs().dir("build/claat");
-                  t.getOutputs().dir("build/site");
-
-                  t.executable(claatPath);
-
-                  t.args("build");
-
-                  t.workingDir("build/site");
-
-                  t.doFirst(
-                      unused ->
-                          project.copy(
-                              copy -> {
-                                copy.from("build/claat", ".firebaserc", "firebase.json");
-                                copy.into("build/site");
-                              }));
+                  t.getDepsVersion().set(1);
+                  t.getOutputDir().set(project.file("build/deps"));
                 });
-    project.getTasks().named("assemble").configure(t -> t.dependsOn(buildSite));
+
+    var assembleSite =
+        project
+            .getTasks()
+            .register(
+                "assembleSite",
+                Copy.class,
+                t -> {
+                  t.dependsOn(exportDocs, downloadDeps);
+
+                  t.from("build/deps", "build/exported");
+                  t.into("build/site");
+                });
+    project.getTasks().named("assemble").configure(t -> t.dependsOn(assembleSite));
   }
 }
