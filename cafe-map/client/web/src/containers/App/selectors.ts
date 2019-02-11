668b3e0ca2ce6c42253f05597939cf1f6a723f06
Choko <choko@curioswitch.org>
2019-02-11 14:56:27 +0900

    First commit of cafe-map client

---
+++ cafe-map/client/web/src/containers/App/selectors.ts
@@ -0,0 +1,50 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019 Choko (choko@curioswitch.org)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+import { RouterState } from 'connected-react-router';
+import { Record } from 'immutable';
+import { createSelector } from 'reselect';
+
+import { GlobalState } from '../../app';
+
+import { State, StateProps } from './reducer';
+
+const selectApp = (state: GlobalState): State => state.app;
+
+export const selectRoute = (state: GlobalState): Record<RouterState> =>
+  state.router;
+
+export interface SelectedProps extends StateProps {
+  readonly route: RouterState;
+}
+
+export default createSelector(
+  selectApp,
+  selectRoute,
+  (substate: State, route: Record<RouterState>): SelectedProps => ({
+    ...substate.toObject(),
+    route: route.toJS(),
+  }),
+);
---
+++ cafe-map/client/web/src/containers/HomePage/saga.ts
@@ -0,0 +1,50 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019 Choko (choko@curioswitch.org)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+import { all, AllEffect, takeLatest } from 'redux-saga/effects';
+import { getType } from 'typesafe-actions';
+
+import actions, { Action } from './actions';
+
+function* doSampleAction() {
+  yield '';
+}
+
+function* unionSaga(action: Action) {
+  switch (action.type) {
+    case getType(actions.sampleAction):
+      yield doSampleAction();
+      break;
+    default:
+  }
+}
+
+export default function* rootSaga(): IterableIterator<AllEffect> {
+  yield all(
+    Object.values(actions).map((action) =>
+      takeLatest(getType(action), unionSaga),
+    ),
+  );
+}
