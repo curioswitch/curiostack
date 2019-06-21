c018471eaf07467a1d88febe3eb59c0b0be67969
Choko <choko@curioswitch.org>
2019-06-21 16:34:55 +0900

    Use get-jdk.sh in CI too for consistency.

--- tools/gradle-plugins/gradle-curiostack-plugin/src/main/resources/curiostack/get-jdk.sh
+++ tools/gradle-plugins/gradle-curiostack-plugin/src/main/resources/curiostack/get-jdk.sh
@@ -1,72 +1,68 @@
 #!/bin/bash
 
-if [ -z "$CI" ]; then
+# OS specific support (must be 'true' or 'false').
+windows=false
+darwin=false
+linux=false
+case "`uname`" in
+  CYGWIN* )
+    windows=true
+    ;;
+  Darwin* )
+    darwin=true
+    ;;
+  MINGW* )
+    windows=true
+    ;;
+  MSYS* )
+    windows=true
+    ;;
+  Linux* )
+    linux=true
+    ;;
+esac
 
-  # OS specific support (must be 'true' or 'false').
-  windows=false
-  darwin=false
-  linux=false
-  case "`uname`" in
-    CYGWIN* )
-      windows=true
-      ;;
-    Darwin* )
-      darwin=true
-      ;;
-    MINGW* )
-      windows=true
-      ;;
-    MSYS* )
-      windows=true
-      ;;
-    Linux* )
-      linux=true
-      ;;
-  esac
+GRADLE_HOME="${GRADLE_USER_HOME:-${HOME}/.gradle}"
 
-  GRADLE_HOME="${GRADLE_USER_HOME:-${HOME}/.gradle}"
+if [ ! -z "$USERPROFILE" ]; then
+  # msys
+  USERPROFILE_CYG=$(cygpath $USERPROFILE)
+  GRADLE_HOME="${USERPROFILE_CYG}/.gradle"
+fi
 
-  if [ ! -z "$USERPROFILE" ]; then
-    # msys
-    USERPROFILE_CYG=$(cygpath $USERPROFILE)
-    GRADLE_HOME="${USERPROFILE_CYG}/.gradle"
-  fi
+OPENJDK_DIR="$GRADLE_HOME/curiostack/openjdk"
 
-  OPENJDK_DIR="$GRADLE_HOME/curiostack/openjdk"
+export JAVA_HOME="$OPENJDK_DIR/jdk-11.0.3+7"
 
-  export JAVA_HOME="$OPENJDK_DIR/jdk-11.0.3+7"
+if "$linux" = "true"; then
+  SRC="https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.3%2B7/OpenJDK11U-jdk_x64_linux_hotspot_11.0.3_7.tar.gz"
+  DEST="$OPENJDK_DIR/OpenJDK11U-jdk_x64_linux_hotspot_11.0.3_7.tar.gz"
+fi
 
-  if "$linux" = "true"; then
-    SRC="https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.3%2B7/OpenJDK11U-jdk_x64_linux_hotspot_11.0.3_7.tar.gz"
-    DEST="$OPENJDK_DIR/OpenJDK11U-jdk_x64_linux_hotspot_11.0.3_7.tar.gz"
-  fi
+if "$darwin" = "true"; then
+  SRC="https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.3%2B7/OpenJDK11U-jdk_x64_mac_hotspot_11.0.3_7.tar.gz"
+  DEST="$OPENJDK_DIR/OpenJDK11U-jdk_x64_mac_hotspot_11.0.3_7.tar.gz"
+  export JAVA_HOME="$JAVA_HOME/Contents/Home"
+fi
 
-  if "$darwin" = "true"; then
-    SRC="https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.3%2B7/OpenJDK11U-jdk_x64_mac_hotspot_11.0.3_7.tar.gz"
-    DEST="$OPENJDK_DIR/OpenJDK11U-jdk_x64_mac_hotspot_11.0.3_7.tar.gz"
-    export JAVA_HOME="$JAVA_HOME/Contents/Home"
+if "$windows" = "true"; then
+  SRC="https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.3%2B7/OpenJDK11U-jdk_x64_windows_hotspot_11.0.3_7.zip"
+  DEST="$OPENJDK_DIR/OpenJDK11U-jdk_x64_windows_hotspot_11.0.3_7.zip"
+fi
+
+if [ ! -d "$JAVA_HOME" ]; then
+  mkdir -p "$OPENJDK_DIR"
+
+  echo "Downloading OpenJDK"
+  if command -v wget >/dev/null 2>&1; then
+    wget -O "$DEST" "$SRC"
+  else
+    curl -L "$SRC" -o "$DEST"
   fi
 
   if "$windows" = "true"; then
-    SRC="https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.3%2B7/OpenJDK11U-jdk_x64_windows_hotspot_11.0.3_7.zip"
-    DEST="$OPENJDK_DIR/OpenJDK11U-jdk_x64_windows_hotspot_11.0.3_7.zip"
+    unzip "$DEST" -d "$OPENJDK_DIR"
+  else
+    tar -xf "$DEST" -C "$OPENJDK_DIR"
   fi
-
-  if [ ! -d "$JAVA_HOME" ]; then
-    mkdir -p "$OPENJDK_DIR"
-
-    echo "Downloading OpenJDK"
-    if command -v wget >/dev/null 2>&1; then
-      wget -O "$DEST" "$SRC"
-    else
-      curl -L "$SRC" -o "$DEST"
-    fi
-
-    if "$windows" = "true"; then
-      unzip "$DEST" -d "$OPENJDK_DIR"
-    else
-      tar -xf "$DEST" -C "$OPENJDK_DIR"
-    fi
-  fi
-
 fi
