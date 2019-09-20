52f0c7f593be2dcb32c39053bb1685739c80569b
Choko <choko@curioswitch.org>
2019-09-20 11:51:51 +0100

    Try 2 2

--- tools/gradle-plugins/gradle-curiostack-plugin/src/test/java/org/curioswitch/gradle/plugins/curiostack/CuriostackPluginTest.java
+++ tools/gradle-plugins/gradle-curiostack-plugin/src/test/java/org/curioswitch/gradle/plugins/curiostack/CuriostackPluginTest.java
@@ -36,12 +36,10 @@ import org.curioswitch.gradle.testing.ResourceProjects;
 import org.gradle.testkit.runner.GradleRunner;
 import org.jsoup.Jsoup;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
 
-@Disabled
 class CuriostackPluginTest {
 
   @SuppressWarnings("ClassCanBeStatic")
