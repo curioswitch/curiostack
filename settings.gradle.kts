6be4ca064bd7f60f6affee863158454bb0776f55
Choko <choko@curioswitch.org>
2019-12-07 20:47:16 +0900

    Update dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -44,7 +44,7 @@ pluginManagement {
 
 plugins {
     id("com.gradle.enterprise").version("3.1")
-    id("org.curioswitch.gradle-curiostack-plugin").version("0.2.0")
+    id("org.curioswitch.gradle-curiostack-plugin").version("0.2.1")
 }
 
 configure<CuriostackExtension> {
