6e1076d6c94941c552d6d5c354667a0514505484
Daniel De Sousa <dan.desousa@gmail.com>
2020-03-31 18:43:07 +0900

    Add mp4 to types. (#529)
    * Add mp4 to types.
    
    * Update common/web/base-web/index.d.ts
    
    * Update common/web/base-web/index.d.ts
    
    * Update common/web/base-web/index.d.ts
    
    Co-Authored-By: Daniel De Sousa <dan.desousa@gmail.com>
    
    Co-authored-by: Choko <choko@curioswitch.org>

--- common/web/base-web/index.d.ts
+++ common/web/base-web/index.d.ts
@@ -46,6 +46,15 @@ declare module '*.svg' {
   export default value;
 }
 
+declare module '*.mp4' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.m4a' {
+  const value: string;
+  export default value;
+}
 declare module '*.eot' {
   const value: string;
   export default value;
