4ca969464927793ac36c6fa4055017929aec2cfa
Choko <choko@curioswitch.org>
2020-07-19 20:49:31 +0900

    Load API key from ignored file.

---
+++ cafe-map/client/unity/Assets/Resources/Secrets/README.txt
@@ -0,0 +1,3 @@
+Secret files that should be in this directory
+
+google-api-key.txt - the Google API key
---
+++ cafe-map/client/unity/Assets/Scripts/KeysInitializer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Google.Maps;
+using UnityEngine;
+
+public class KeysInitializer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        var googleApiKey = readSecret("google-api-key");
+        foreach (var map in GameObject.FindObjectsOfType<MapsService>())
+        {
+            map.ApiKey = googleApiKey;
+        }
+    }
+
+    private static string readSecret(string filename)
+    {
+        var file = Resources.Load<TextAsset>($"Secrets/{filename}");
+        return file.text.Trim();
+    }
+}
