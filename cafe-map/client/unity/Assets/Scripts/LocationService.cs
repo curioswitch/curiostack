7bab888265407b666e9d67147861b72fd64230ba
Choko <choko@curioswitch.org>
2020-09-12 16:45:38 +0900

    Instantiate models for certain feature types.

---
+++ cafe-map/client/unity/Assets/Scripts/LocationService.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CafeMap.Player
+{
+    public class LocationService : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
