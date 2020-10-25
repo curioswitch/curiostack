69645b489a1082322fb21ad8ca02e6aa8fe829ae
Choko <choko@curioswitch.org>
2020-10-25 15:15:30 +0900

    Add links to instagram / maps for results.

---
+++ cafe-map/client/unity/Assets/Scripts/Map/WebsiteLink.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class WebsiteLink : MonoBehaviour, IPointerClickHandler
+{
+
+    private string url;
+
+    public void SetUrl(string url)
+    {
+        this.url = url;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Application.OpenURL(url);
+    }
+}
