f07837655c98c63d13e5e8e74b587a99d36043f2
Choko <choko@curioswitch.org>
2020-10-31 22:39:02 +0900

    Current location button (#599)
    * Gitmerge (#585)
    
    * Buildings
    
    * wip
    
    * Add GitHub plugin
    
    * Remove GitHub
    
    * No merge unity
    
    * Add gitmerge
    
    Co-authored-by: skopswitch <skop@curioswitch.org>
    
    * UI変更
    
    * Ui変更
    
    * rocekt
    
    * 現在地ボタン追加
    
    * Add location button
    
    Co-authored-by: skopswitch <skop@curioswitch.org>

---
+++ cafe-map/client/unity/Assets/Scripts/Map/LocationButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using CafeMap.Player.Services;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Zenject;
+
+public class LocationButton : MonoBehaviour, IPointerClickHandler
+{
+
+    private ViewportService _viewportService;
+
+    [Inject]
+    public void Init(ViewportService viewportService)
+    {
+        _viewportService = viewportService;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        var location = CafeMap.Player.LocationService.getLocation();
+        _viewportService.SetCenter(location.Lat, location.Lng);
+    }
+}
