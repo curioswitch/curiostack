8b755fc5c8e701f8ed759bbf334f0fa66b53c00d
Daniel De Sousa <dan.desousa@gmail.com>
2021-03-03 09:32:12 +0900

    Add jgit ssh jsch bundle dep. (#618)

--- tools/curiostack-bom/build.gradle.kts
+++ tools/curiostack-bom/build.gradle.kts
@@ -284,7 +284,7 @@ val DEPENDENCY_SETS = listOf(
         DependencySet(
                 "org.eclipse.jgit",
                 "5.10.0.202012080955-r",
-                listOf("org.eclipse.jgit")
+                listOf("org.eclipse.jgit, org.eclipse.jgit.ssh.jsch")
         ),
         DependencySet(
                 "org.immutables",
