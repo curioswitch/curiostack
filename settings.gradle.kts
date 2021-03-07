702c9cb65e0a00fe5865303f604507c8f5d92592
Choko <choko@curioswitch.org>
2021-03-07 16:20:01 +0900

    Update mac os sdk version for go 1.16 compatibility

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -46,7 +46,7 @@ pluginManagement {
 
 plugins {
     id("com.gradle.enterprise").version("3.5.2")
-    id("org.curioswitch.gradle-curiostack-plugin").version("0.7.0")
+    id("org.curioswitch.gradle-curiostack-plugin").version("0.7.1")
 }
 
 configure<CuriostackExtension> {
