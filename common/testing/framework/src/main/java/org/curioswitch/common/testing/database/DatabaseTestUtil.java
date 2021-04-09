5e2c931eedb58d3e811b282f1810d679cc6e22a6
Bianca Woo <18028868+woobianca@users.noreply.github.com>
2021-04-09 09:55:55 +0900

    Remove SimpleFlatMapper record mapper from database + test db configurations (#621)
    * Remove DatabaseUtil
    
    * Remove SFM record mapper db config
    
    * Remove SFM record mapper test db config
    
    * Add newDbContext override method with Clock
    
    * Spotless

--- common/testing/framework/src/main/java/org/curioswitch/common/testing/database/DatabaseTestUtil.java
+++ common/testing/framework/src/main/java/org/curioswitch/common/testing/database/DatabaseTestUtil.java
@@ -34,7 +34,7 @@ import static org.mockito.Mockito.withSettings;
 
 import com.google.common.collect.ImmutableList;
 import java.sql.SQLException;
-import org.curioswitch.common.server.framework.database.DatabaseUtil;
+import java.time.Clock;
 import org.jooq.DSLContext;
 import org.jooq.Record;
 import org.jooq.Result;
@@ -71,9 +71,16 @@ public final class DatabaseTestUtil {
   public static DSLContext newDbContext(MockDataProvider dataProvider) {
     MockConnection connection = new MockConnection(dataProvider);
     DSLContext db = DSL.using(connection, SQLDialect.MYSQL);
-    db.configuration().set(DatabaseUtil.sfmRecordMapperProvider());
     db.settings().setRenderSchema(false);
     return db;
+  }
+
+  /**
+   * Returns a {@link DSLContext} with a mock connection using the provided {@link MockDataProvider}
+   * and {@link Clock} to override the system's current time with a fixed timestamp.
+   */
+  public static DSLContext newDbContext(MockDataProvider dataProvider, Clock clock) {
+    return newDbContext(dataProvider).configuration().set(clock).dsl();
   }
 
   /**
